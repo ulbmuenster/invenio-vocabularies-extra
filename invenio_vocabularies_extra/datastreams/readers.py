aebe3ce278392287b32042e40a3868e7970d3bbf
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2025-07-16 15:22:10 +0200

    Add info to docstring

--- invenio_vocabularies_extra/datastreams/readers.py
+++ invenio_vocabularies_extra/datastreams/readers.py
@@ -44,6 +44,7 @@ class WikidataAffiliationsReader(SPARQLReader):
 
         :param search_space: Additional conditions for which items should be retrieved from wikidata.
         :param mode: Mode of operation (default is 'r' for reading).
+        :param client_params: Additional client parameters to pass to the SPARQL client.
         """
         origin = "https://query.wikidata.org/sparql"
 
